e829a89e25ffd599326b05f74c4d820b70839387
GFMws <ydalite01@gmail.com>
2025-01-10 16:42:44 +0400

    ?

--- python/server.py
+++ python/server.py
@@ -12,7 +12,7 @@ from data_split_class import LData
 from MDataBase import Techno
 import Config
 
-version = "1.0"
+version = "1.1"
 
 HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
 PORT = 11200      # Port to listen on (non-privileged ports are > 1023)
