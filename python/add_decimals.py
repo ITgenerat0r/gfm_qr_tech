fee07b584009c80b61ae020daa1a52bc32357a7d
GFMws <ydalite01@gmail.com>
2025-01-10 14:25:00 +0400

    parser

---
+++ python/add_decimals.py
@@ -0,0 +1,26 @@
+from MDataBase import Techno
+import Config
+from includes import *
+
+db = Techno(Config.host, Config.user, Config.password, Config.db_name)
+db.connect()
+db.set_time_out(DB_timeout)
+
+
+
+with open("raw_data.txt") as file:
+    for line in file:
+    	ind = line.rstrip().rfind("МКЦБ.")
+        if ind >= 0:
+        	mkcb = line.rstrip()[ind:]
+        	name = line[:ind-1].rstrip()
+        	tp = ""
+        	lk = name.find('"')
+        	rk = name.rfind('"')
+        	if lk >= 0 and rk >= 0:
+        		tp = name[lk:rk]
+        		if "Кедр" in tp:
+        			tp = tp[5:]
+        	print(yellow_text(f"{mkcb}:{tp}({name})"))
+        else:
+        	print(red_text(line.rstrip()))
